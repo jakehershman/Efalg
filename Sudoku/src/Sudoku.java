bf58ccb62160cceae5f302847c41683a4fb0a6ae
danielguerber <guerbi1990@gmx.ch>
2013-09-24 19:21:34 +0200

    Working Sudoku

--- Sudoku/src/Sudoku.java
+++ Sudoku/src/Sudoku.java
@@ -5,48 +5,69 @@ import java.util.List;
 public class Sudoku {
 	private int[][] cells;
 	private int blockSize;
+	private int size;
 	LinkedList<int[][]> solutions;
+	
+
 	
 	public Sudoku(int blockSize, int[][] cells) {
 		this.blockSize = blockSize;
+		this.size = blockSize*blockSize;
 		this.cells = cells;
 	}
 	
 	public List<int[][]> getSolutions() {
 		 solutions = new  LinkedList<int[][]>();
-		 fillNextCell(-1,0);
+		 fillNextCell(0,0);
 		 return solutions;
 	}
 	
 	private void fillNextCell(int x, int y) {
-		int nextX = x+1;
-		int nextY = y;
-		if(nextX >= cells.length) {
-			nextX = 0;
-			nextY = y+1;
+		if(x >= cells.length) {
+			x = 0;
+			y++;
 		}
 		
-		if(nextY >= cells.length) {
-			if (checkSanity())
-				solutions.add(cells.clone());
+		//System.out.println(x + " " + y);
+		
+		if(y >= cells.length) {
+			//if (checkSanity())
+				solutions.add(cloneCells());
 		} else {
-			if(cells[nextX][nextY] == 0) {
-				boolean[] fixedNumbers = new boolean[blockSize*blockSize+1];
-				markRowFixedNumbers(nextX, fixedNumbers);
-				markColumnFixedNumbers(nextY, fixedNumbers);
-				markBlockFixedNumbers(nextX%blockSize, nextY/blockSize, fixedNumbers);
-				for (int i = 1; i <= blockSize*blockSize; i++) {
-					if (!fixedNumbers[i]) {
-						cells[nextX][nextY] = i;
-						fillNextCell(nextX,nextY);
+			if(cells[x][y] == 0) {
+				for (int i = 1; i <= size; i++) {
+					if (isPossible(x, y, i)) {
+						cells[x][y] = i;
+						fillNextCell(x+1,y);
+						cells[x][y] = 0;
 					}
 				}
-				cells[nextX][nextY] = 0;
+				
 			} else {
-				fillNextCell(nextX, nextY);
+				fillNextCell(x+1, y);
 			}
 			
 		} 
+	}
+	
+	private boolean isPossible(int x, int y, int number) {
+		for (int i = 0; i < size; i++) {
+			if (cells[x][i]==number || cells[i][y]==number)
+				return false;
+		}
+		
+		int blockX = blockSize*(x/blockSize);
+		int blockY = blockSize*(y/blockSize);
+		
+		for (int i = 0; i < blockSize; i++) {
+			for (int j = 0; j < blockSize; j++) {
+				if (cells[blockX+i][blockY+j]==number) {
+					return false;
+				}
+			}
+		}
+		
+		return true;
 	}
 	
 	private int[] findEmptyCell() {
@@ -60,7 +81,7 @@ public class Sudoku {
 	}
 	
 	private boolean checkSanity() {
-		for (int i = 0; i < blockSize*blockSize; i++) {
+		for (int i = 0; i < size; i++) {
 			if (!(checkColumnSanity(i) && checkRowSanity(i) && checkBlockSanity(i)))
 				return false;
 		}
@@ -68,7 +89,7 @@ public class Sudoku {
 	}
 	
 	private boolean checkColumnSanity(int column) {
-		boolean[] fixedNumbers = new boolean[blockSize*blockSize+1];
+		boolean[] fixedNumbers = new boolean[size+1];
 		for (int i = 0; i < cells.length; i++) {
 			int number = cells[i][column];
 			if (number != 0 && fixedNumbers[number])
@@ -80,7 +101,7 @@ public class Sudoku {
 	}
 	
 	private boolean checkBlockSanity(int block) {
-		boolean[] fixedNumbers = new boolean[blockSize*blockSize+1];
+		boolean[] fixedNumbers = new boolean[size+1];
 		int x = block % blockSize;
 		int y = block / blockSize;
 		for (int i = 0; i < blockSize; i++) {
@@ -96,7 +117,7 @@ public class Sudoku {
 	}
 	
 	private boolean checkRowSanity(int row) {
-		boolean[] fixedNumbers = new boolean[blockSize*blockSize+1];
+		boolean[] fixedNumbers = new boolean[size+1];
 		for (int i = 0; i < cells[row].length; i++) {
 			int number = cells[row][i];
 			if (number != 0 && fixedNumbers[number])
@@ -120,7 +141,7 @@ public class Sudoku {
 	}
 	
 	private boolean markBlockFixedNumbers(int x, int y, boolean[] fixedNumbers) {
-//		boolean[] fixedNumbers = new boolean[blockSize*blockSize+1];
+//		boolean[] fixedNumbers = new boolean[size+1];
 //		int x = block % blockSize;
 //		int y = block / blockSize;
 		for (int i = 0; i < blockSize; i++) {
@@ -144,27 +165,65 @@ public class Sudoku {
 				{8,0,0,0,0,0,0,2,0},
 				{0,7,0,0,1,0,5,0,0},
 				{4,0,0,0,0,5,3,0,0},
-				{0,1,0,0,7,0,0,0,6},
+				{0,1,0,0,4,0,0,0,6},
 				{0,0,3,2,0,0,0,8,0},
 				{0,6,0,5,0,0,0,0,9},
 				{0,0,4,0,0,0,0,3,0},
 				{0,0,0,0,0,9,7,0,0}};
 		
 		int[][] cells2 = {
-				{1,2,3,4,5,6,7,8,9},
+				{0,3,0,0,0,4,0,0,0},
+				{0,0,0,6,0,0,0,8,5},
 				{0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0}};
+				{0,0,9,5,0,6,4,0,1},
+				{0,8,0,0,1,0,0,0,0},
+				{0,1,0,7,0,0,6,2,0},
+				{7,0,5,0,0,0,0,0,0},
+				{0,0,0,0,0,3,1,0,4},
+				{0,0,0,0,0,0,0,6,2}};
+		
+		int[][] cells4 = {
+				{1,2,3,4,5,0,7,0,9},
+				{8,6,4,9,7,1,5,3,2},
+				{9,7,5,3,2,0,4,0,1},
+				{7,8,9,2,3,5,1,4,6},
+				{5,3,6,7,1,4,9,2,8},
+				{4,1,2,8,6,9,3,7,5},
+				{6,9,8,5,4,7,2,1,3},
+				{3,5,7,1,8,2,6,9,4},
+				{2,4,1,6,9,3,8,5,7}};
 		
 		int[][] cells3 = new int[N*N][N*N];
-	
-		Sudoku s = new Sudoku(N,cells2);
+		Sudoku s = new Sudoku(N,cells4);
 		List<int[][]> solutions = s.getSolutions();
 		System.out.println(solutions.size());
+		if(solutions.size()>0)
+			Sudoku.printCells(solutions.get(0));
+	}
+	
+	public static void printCells(int[][] cells) {
+		
+		for (int x = 0; x < cells.length; x++) {
+			StringBuilder sb = new StringBuilder("[");
+			for (int y = 0; y < cells[x].length; y++) {
+				sb.append(cells[x][y]);
+				sb.append(",");
+			}
+			sb.deleteCharAt(sb.length()-1);
+			sb.append("]");
+			System.out.println(sb);
+		}
+		System.out.println();
+		
+	}
+	
+	private int[][] cloneCells() {
+		int[][] newCells = new int[size][size];
+		for (int x = 0; x < cells.length; x++) {
+			for (int y = 0; y < cells[x].length; y++) {
+				newCells[x][y] = cells[x][y];
+			}
+		}
+		return newCells;
 	}
 }
